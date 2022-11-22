ce38d8862c568732a32826d5e098890d17a9592d
meohyeon <theogus2@naver.com>
2022-11-22 21:49:52 +0900

    알고리즘 공부 프로그래머스 k번쨰수

--- py01.py
+++ py01.py
@@ -1,7 +1,8 @@
+#알고리즘 공부
 #프로그래머스: 코딩테스트 연습 > 정렬 > K번쨰수
 array=[1, 5, 2, 6, 3, 7, 4]
 commands=[[2,5,3],[4,4,1],[1,7,3]]
-anwer=[]
+answer=[]
 
 
 
@@ -10,10 +11,7 @@ def Operator():
         a=[]
         a=array[commands[i][0]-1:commands[i][1]]
         a.sort()
-        anwer.append(a[commands[i][2]-1])
-  
-
-
+        answer.append(a[commands[i][2]-1])
 Operator()
-print(anwer)
+print(answer)
 
