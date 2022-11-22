6117028ca562532868fe3e9b96013f8af4a1a932
meohyeon <theogus2@naver.com>
2022-11-22 21:47:40 +0900

    프로그래머스 k번쨰수

--- py01.py
+++ py01.py
@@ -1,6 +1,19 @@
-print("hello")
-a=10
-print(a)
-b=10
-print(b)
+#프로그래머스: 코딩테스트 연습 > 정렬 > K번쨰수
+array=[1, 5, 2, 6, 3, 7, 4]
+commands=[[2,5,3],[4,4,1],[1,7,3]]
+anwer=[]
+
+
+
+def Operator():
+    for i in range(len(commands)):
+        a=[]
+        a=array[commands[i][0]-1:commands[i][1]]
+        a.sort()
+        anwer.append(a[commands[i][2]-1])
+  
+
+
+Operator()
+print(anwer)
 
