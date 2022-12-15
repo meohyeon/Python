6dde0cc9f7087f0fb9bebf42288492e475bc7494
meohyeon <theogus2@naver.com>
2022-12-15 19:03:21 +0900

    프로그래머스 n queen

---
+++ py25.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+def solution(n):
+    lis = [0] * (n+1)
+    return recol(0,n,lis)
+def recol(a,n,lis):
+    answer = 0
+    if a == n:   
+        return 1
+    else:
+        for i in range(n):
+            lis[a] = i
+            if check(a,lis):
+                answer += recol(a+1,n,lis)
+    return answer
+def check(a,lis):
+    for i in range(a):
+        if lis[a] == lis[i] or (a-i == abs(lis[a]-lis[i])):
+            return False
+    return True
+print(solution(n))
