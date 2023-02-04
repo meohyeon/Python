dfbe95a183a1b1ee1b3c1ed01b852ca9376a2fff
meohyeon <theogus2@naver.com>
2023-02-04 12:27:04 +0900

    프로그래머스 문자열 마음대로 정렬

--- py74.py
+++ py74.py
@@ -1,4 +1,7 @@
 #프로그래머스 문자열 마음대로 정렬 
-strings = ["sun", "bed", "car"]
-n = 1
-print(sorted(strings,key=lambda x:(x[n],x)))
+strings1 = ["sun", "bed", "car"]
+strings2 = ["abce", "abcd", "cdx"]
+n1 = 1
+n2 = 2
+print(sorted(strings1,key=lambda x:(x[n1],x)))
+print(sorted(strings2,key=lambda x:(x[n2],x)))
