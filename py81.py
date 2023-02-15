7a9455760d8562341a7ac630fca362ed8fe6990b
meohyeon <theogus2@naver.com>
2023-02-15 20:06:50 +0900

    프로그래머스 최소 직사각형

--- py81.py
+++ py81.py
@@ -1,5 +1,9 @@
 # 프로그래머스 최소 직사각형 
-test = [[60, 50], [30, 70], [60, 30], [80, 40]]
-a = [min(x) for x in test]
-b = [max(x) for x in test]
-print(max(a)*max(b))
+test1 = [[60, 50], [30, 70], [60, 30], [80, 40]]
+test2 = [[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]	
+test3 = [[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]
+test = [test1,test2,test3]
+for i in test:
+    a = [min(x) for x in i]
+    b = [max(x) for x in i]
+    print(max(a)*max(b))
