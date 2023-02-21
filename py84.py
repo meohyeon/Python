fd13cdf184c45e63e5a695960cb49666100077fb
meohyeon <theogus2@naver.com>
2023-02-21 20:36:45 +0900

    프로그래머스 행렬의 곱셉 level 2

--- py84.py
+++ py84.py
@@ -6,3 +6,7 @@ for i in range(len(arr1)):
         for k in range(len(arr1[0])):
             answer[i][j] += arr1[i][k]*arr2[k][j]
 print(answer)
+
+#zip 이용한 방법  zip(*arr2) ex) 열만 추출 
+answer2 = [[sum(x*y for x,y in zip(arr1_row,arr2_col))for arr2_col in zip(*arr2)]for arr1_row in arr1]
+print(answer2)
